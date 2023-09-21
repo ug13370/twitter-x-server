399edc27231fc52a1bbca12d73e43817eb76f30d
Utkarsh Gupta <utkarsh13370@gmail.com>
2023-09-21 20:37:26 +0530

    Consoling Mongo Db URL

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -4,5 +4,8 @@ const options: any = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
+console.log("Mongo DB URL :- " + process.env.MONGODB_URL);
+
 const uri: string = process.env.MONGODB_URL || "";
 mongoose.connect(uri, options);
